803a2f2c7075cb7dea5be4012d13985c50ba1e2a
gonzalojparra <gonzzaparra@gmail.com>
2022-04-22 20:04:16 -0300

    Mostrar tabla

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
 
+import { Country } from '../../interfaces/pais.interface';
+
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
@@ -11,6 +13,7 @@ export class PorPaisComponent {
 
   termino : string = '';
   hayError: boolean = false;
+  paises  : Country[] = [];
   
   constructor( private paisService: PaisService ) { }
 
@@ -19,10 +22,13 @@ export class PorPaisComponent {
     console.log(this.termino);
 
     this.paisService.buscarPais( this.termino )
-      .subscribe( (resp) => {
-        console.log(resp);
+      .subscribe( (paises) => {
+        console.log(paises);
+        this.paises = paises;
+
       }, (err) => {
         this.hayError = true;
+        this.paises   = [];
       });
   }
 
